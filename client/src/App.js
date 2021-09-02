04d8776e1b1e37609ebd434766dbca4cc5684141
Tamira <t.gunzburg@gmail.com>
2021-09-02 17:52:57 +0200

    Basic login functionality on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Login } from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,86 @@
+// Component that renders a login form
+
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+export const Login = (props) => {
+  // Use history to be able to link to other Routes.
+  const history = useHistory();
+
+  // Declare states purely to control the form elements.
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Single handler for all controlled form elements, updates the corresponding
+  // state depending on which field was typed in.
+  const handleChange = (event) => {
+    const currentValue = event.target.value;
+    if (event.target.name === "email") {
+      setEmail(currentValue);
+    } else if (event.target.name === "password") {
+      setPassword(currentValue);
+    }
+  };
+
+  // Handler for when the final form is submitted.
+  const handleSubmit = async (event) => {
+    // Prevent the browser from re-loading the page
+    event.preventDefault();
+
+    // Call the server with the different states as arguments.
+    const url = "http://localhost:3001/login";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email: email, password: password }),
+    });
+    const jsonResponse = await response.json();
+
+    if (jsonResponse.err) {
+      //Display an alert to the user?
+      console.log(jsonResponse.err);
+    }
+    if (jsonResponse.token) {
+      //store jwt and user info in sessionStrorage
+      sessionStorage.setItem("user", JSON.stringify(jsonResponse));
+      console.log("Success", jsonResponse.token);
+      // Reset the states back to empty, so that the form looks blank again.
+      setEmail("");
+      setPassword("");
+
+      // Redirect the user to the next page.
+      //ADAPT THIS
+      history.push("/");
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <br></br>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          required
+        ></input>
+        <br></br>
+        <label htmlFor="password">Password:</label>
+        <br></br>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={password}
+          onChange={handleChange}
+          requried
+        ></input>
+        <br></br>
+        <input type="submit" value="Login"></input>
+      </form>
+    </div>
+  );
+};
