779a1df9878814ea5a236b04223f6610a6b5da75
Tamira <t.gunzburg@gmail.com>
2021-09-04 18:24:15 +0200

    Add credentials db and endpoints on server side

---
+++ routes/credentials.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const credentialControllers = require("../controllers/Credentials.controller");
+const auth = require("../middleware/authenticationMiddleware");
+
+// Endpoint to get all the credentials for a given division:
+
+router.post(
+  "/",
+  auth.authenticateNormal,
+  credentialControllers.listAllCredentials
+);
+
+// Endpoint to get a single credential, so it can be displayed and edited:
+
+router.post(
+  "/display",
+  auth.authenticateManager,
+  credentialControllers.findCredential,
+  (req, res) => {
+    res.send(req.credential);
+  }
+);
+
+// Endpoint to update an existing credential:
+
+router.post(
+  "/update",
+  auth.authenticateManager,
+  credentialControllers.updateCredential
+);
+
+// Endpoint to create a new credential:
+
+router.post(
+  "/create",
+  auth.authenticateManager,
+  credentialControllers.addCredential
+);
+
+module.exports = router;
